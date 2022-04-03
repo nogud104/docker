cd83ff6b07ebdb31f2a4f6f5419fa9a548135deb
Chuvashov Mikhail <raven4ster@gmail.com>
2022-04-04 00:50:30 +0300

    add env  django

--- lib_catalog/lib_catalog/settings.py
+++ lib_catalog/lib_catalog/settings.py
@@ -81,9 +81,9 @@ WSGI_APPLICATION = 'lib_catalog.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "django",
-        "USER": "django",
-        "PASSWORD": "django",
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
         "HOST": "database",
         "PORT": "5432",
         "CONN_MAX_AGE": None
